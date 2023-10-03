4127de6f7b6d443d11af8302ef98f66fded05b86
jiito <benjamin.allanrahill@gmail.com>
2023-10-03 16:05:59 -0700

    update metaphor SDK

--- fridgeflix-backend/server.js
+++ fridgeflix-backend/server.js
@@ -3,12 +3,14 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import { config } from "dotenv";
 import OpenAI from "openai";
-import axios from "axios";
+import Metaphor from "metaphor-node";
+
+const metaphor = new Metaphor(process.env.METAPHOR_API_KEY);
 
 config();
 
 const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY
+  apiKey: process.env.OPENAI_API_KEY,
 });
 
 const app = express();
@@ -17,6 +19,17 @@ const PORT = process.env.PORT || 9000;
 initializeMiddlewares();
 initializeServer();
 
+app.get("/search", async (req, res) => {
+  const searchTerm = req.query.searchTerm;
+  const query = `Here is the best recipe for ${searchTerm}:`;
+  const response = await metaphor.search(query, {
+    numResults: 5,
+    useAutoprompt: true,
+    type: "neural",
+  });
+  const links = response.results.map((result) => result.url);
+  return res.json({ links });
+});
 app.post("/search", handleSearchRequest);
 
 function initializeMiddlewares() {
@@ -31,53 +44,47 @@ function initializeServer() {
 }
 
 async function handleSearchRequest(req, res) {
-    console.log("Search request received");
+  console.log("Search request received");
 
-    const searchTerm = req.body.searchTerm;
-    try {
-        const dishNames = await fetchDishNamesFromOpenAI(searchTerm);
-        const recipeLinks = await fetchRecipeLinksForDishes(dishNames);
+  const searchTerm = req.body.searchTerm;
+  try {
+    const dishNames = await fetchDishNamesFromOpenAI(searchTerm);
+    const recipeLinks = await fetchRecipeLinksForDishes(dishNames);
 
-        res.json({ metaphorResults: recipeLinks });
-    } catch (error) {
-        console.error("Error processing the search request:", error.message);
-        res.status(500).json({ error: "Error processing the request." });
-    }
+    res.json({ metaphorResults: recipeLinks });
+  } catch (error) {
+    console.error("Error processing the search request:", error.message);
+    res.status(500).json({ error: "Error processing the request." });
+  }
 }
 
 async function fetchDishNamesFromOpenAI(searchTerm) {
-    const prompt = `What made with ${searchTerm} and other spices found commonly in US kitchens, give list of 6 top dishes sorted by popularity`;
-    const response = await openai.chat.completions.create({
-        model: "gpt-3.5-turbo",
-        messages: [{ role: "user", content: prompt }]
-    });
+  const prompt = `What made with ${searchTerm} and other spices found commonly in US kitchens, give list of 6 top dishes sorted by popularity`;
+  const response = await openai.chat.completions.create({
+    model: "gpt-3.5-turbo",
+    messages: [{ role: "user", content: prompt }],
+  });
 
-    const content = response.choices[0].message.content;
-    return [...content.matchAll(/\d+\.\s(.*?):/g)].map(match => match[1]);
+  const content = response.choices[0].message.content;
+  return [...content.matchAll(/\d+\.\s(.*?):/g)].map((match) => match[1]);
 }
 
 async function fetchRecipeLinksForDishes(dishNames) {
-    const recipeLinks = [];
-    
-    for (const dish of dishNames) {
-        const query = `Here is the best recipe for ${dish}:`;
-        const response = await axios.post('https://api.metaphor.systems/search', {
-            query,
-            numResults: 5,
-            useAutoprompt: true,
-            type: "neural"
-        }, {
-            headers: {
-                'x-api-key': process.env.METAPHOR_API_KEY,
-                'Content-Type': 'application/json'
-            }
-        });
-        
-        const links = response.data.results.map(result => result.url);
-        recipeLinks.push({ [dish]: links });
-    }
+  const recipeLinks = [];
 
-    return recipeLinks;
+  for (const dish of dishNames) {
+    const query = `Here is the best recipe for ${dish}:`;
+    const response = await metaphor.search(query, {
+      numResults: 5,
+      useAutoprompt: true,
+      type: "neural",
+    });
+
+    const links = response.data.results.map((result) => result.url);
+    recipeLinks.push({ [dish]: links });
+  }
+
+  return recipeLinks;
 }
 
 export default app;
