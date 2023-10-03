a2c81f17317040199f1e6ccbf2142046eeaf8b46
jiito <benjamin.allanrahill@gmail.com>
2023-10-03 16:30:49 -0700

    w/o formatting

--- fridgeflix-backend/server.js
+++ fridgeflix-backend/server.js
@@ -3,14 +3,14 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import { config } from "dotenv";
 import OpenAI from "openai";
-import Metaphor from "metaphor-node";
+import Metaphor from "metaphor-node"
 
 const metaphor = new Metaphor(process.env.METAPHOR_API_KEY);
 
 config();
 
 const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey: process.env.OPENAI_API_KEY
 });
 
 const app = express();
@@ -33,47 +33,43 @@ function initializeServer() {
 }
 
 async function handleSearchRequest(req, res) {
-  console.log("Search request received");
+    console.log("Search request received");
 
-  const searchTerm = req.body.searchTerm;
-  try {
-    const dishNames = await fetchDishNamesFromOpenAI(searchTerm);
-    const recipeLinks = await fetchRecipeLinksForDishes(dishNames);
+    const searchTerm = req.body.searchTerm;
+    try {
+        const dishNames = await fetchDishNamesFromOpenAI(searchTerm);
+        const recipeLinks = await fetchRecipeLinksForDishes(dishNames);
 
-    res.json({ metaphorResults: recipeLinks });
-  } catch (error) {
-    console.error("Error processing the search request:", error.message);
-    res.status(500).json({ error: "Error processing the request." });
-  }
+        res.json({ metaphorResults: recipeLinks });
+    } catch (error) {
+        console.error("Error processing the search request:", error.message);
+        res.status(500).json({ error: "Error processing the request." });
+    }
 }
 
 async function fetchDishNamesFromOpenAI(searchTerm) {
-  const prompt = `What made with ${searchTerm} and other spices found commonly in US kitchens, give list of 6 top dishes sorted by popularity`;
-  const response = await openai.chat.completions.create({
-    model: "gpt-3.5-turbo",
-    messages: [{ role: "user", content: prompt }],
-  });
+    const prompt = `What made with ${searchTerm} and other spices found commonly in US kitchens, give list of 6 top dishes sorted by popularity`;
+    const response = await openai.chat.completions.create({
+        model: "gpt-3.5-turbo",
+        messages: [{ role: "user", content: prompt }]
+    });
 
-  const content = response.choices[0].message.content;
-  return [...content.matchAll(/\d+\.\s(.*?):/g)].map((match) => match[1]);
+    const content = response.choices[0].message.content;
+    return [...content.matchAll(/\d+\.\s(.*?):/g)].map(match => match[1]);
 }
 
 async function fetchRecipeLinksForDishes(dishNames) {
-  const recipeLinks = [];
+    const recipeLinks = [];
+    
+    for (const dish of dishNames) {
+        const query = `Here is the best recipe for ${dish}:`;
+        const response = await metaphor.search(query, {numResults: 5, useAutoprompt: true, type: "neural"})
+        
+        const links = response.data.results.map(result => result.url);
+        recipeLinks.push({ [dish]: links });
+    }
 
-  for (const dish of dishNames) {
-    const query = `Here is the best recipe for ${dish}:`;
-    const response = await metaphor.search(query, {
-      numResults: 5,
-      useAutoprompt: true,
-      type: "neural",
-    });
-
-    const links = response.data.results.map((result) => result.url);
-    recipeLinks.push({ [dish]: links });
-  }
-
-  return recipeLinks;
+    return recipeLinks;
 }
 
 export default app;
